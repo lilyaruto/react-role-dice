f35ee4980155cfcb473da8e3f80971182b43a60c
Yaruto <neo960211@gmail.com>
2023-06-25 18:07:42 +0200

    Added animation for rolling dices

--- role-dice/src/Dice.js
+++ role-dice/src/Dice.js
@@ -5,7 +5,7 @@ class Dice extends Component {
     render() {
         return(
             <div className="dice_box">
-                <i className={`fas fa-10x fa-dice-${this.props.num}`}></i>
+                <i className={`fas fa-10x fa-dice-${this.props.num} ${this.props.wiggle ? "wiggle-dice" : ""}`}></i>
             </div>
         );
     }
--- role-dice/src/RoleDice.js
+++ role-dice/src/RoleDice.js
@@ -11,7 +11,8 @@ class RoleDice extends Component {
         super(props);
         this.state = {
             dice1: "one",
-            dice2: "one"
+            dice2: "one",
+            isRolling: false
         }
         this.roll = this.roll.bind(this)
     }
@@ -19,16 +20,20 @@ class RoleDice extends Component {
     roll() {
         this.setState({dice1: this.props.face[Math.floor(Math.random() * this.props.face.length)]});
         this.setState({dice2: this.props.face[Math.floor(Math.random() * this.props.face.length)]});
+        this.setState({isRolling: true});
+        setTimeout(()=> {
+            this.setState({isRolling: false});
+        }, 1000);
     }
 
     render() {
         return(
             <div className="role_box">
                 <div className="dices_container">
-                    <Dice num={this.state.dice1}/>
-                    <Dice num={this.state.dice2}/>
+                    <Dice num={this.state.dice1} wiggle={this.state.isRolling}/>
+                    <Dice num={this.state.dice2} wiggle={this.state.isRolling}/>
                 </div>
-                <button onClick={this.roll} className="role-button">Role dices</button>
+                <button onClick={this.roll} className="role-button" disabled={this.state.isRolling}>{this.state.isRolling ? "Rolling..." : "Role dices"}</button>
             </div>
         );
     }
